4ad7a1506475e0f9cdbdd07f46d30d3e476617d2
Maria <36799416+Mashka241@users.noreply.github.com>
2018-06-15 22:26:32 +0300

    добавила пропущенные ; и пробелы, переименовала повторяющиеся переменные

--- js/pictures.js
+++ js/pictures.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var photoComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.',
-'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var photoComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
 var photoDescription = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
 
-var getRandomNumber = function (x,y) {
+var getRandomNumber = function (x, y) {
   var number = Math.floor(x + ((Math.random() * (y - x))));
   return number;
 };
@@ -20,7 +19,7 @@ var compareRandom = function () {
 };
 
 var getUnorderedArray = function (num) {
-  var newArray = []
+  var newArray = [];
   for (var i = 1; i <= num; i++) {
     newArray.push(i);
   }
@@ -28,18 +27,18 @@ var getUnorderedArray = function (num) {
   return newArray;
 };
 
-var createRandomPhoto = function (photoNumber, comments, description) {
-  var url = 'photos/' + photoNumber +'.jpg';
+var createRandomPhoto = function (photoNumber, photoComment, photoDescription) {
+  var url = 'photos/' + photoNumber + '.jpg';
   var likes = getRandomNumber(15, 200);
-  var comments = getRandomArrElement(comments);
-  var description = getRandomArrElement(description);
+  var comments = getRandomArrElement(photoComment);
+  var description = getRandomArrElement(photoDescription);
   var randomPhoto = {
     url: url,
     likes: likes,
     comments: comments,
     description: description
   };
-  return randomPhoto;  
+  return randomPhoto;
 };
 
 var createPhotosArray = function (numberOfPhotos) {
