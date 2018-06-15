0312679cd0616d48d1ed953112acec9b98a0b45d
Maria <36799416+Mashka241@users.noreply.github.com>
2018-06-15 22:29:55 +0300

    еще одну переменную переименовала

--- js/pictures.js
+++ js/pictures.js
@@ -2,7 +2,7 @@
 
 var photoComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-var photoDescription = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
+var photoDescriptions = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
 
 var getRandomNumber = function (x, y) {
   var number = Math.floor(x + ((Math.random() * (y - x))));
@@ -45,7 +45,7 @@ var createPhotosArray = function (numberOfPhotos) {
   var photosArray = [];
   var photoUrl = getUnorderedArray(numberOfPhotos);
   for (var i = 0; i < numberOfPhotos; i++) {
-    var newPhoto = createRandomPhoto(photoUrl[i], photoComments, photoDescription);
+    var newPhoto = createRandomPhoto(photoUrl[i], photoComments, photoDescriptions);
     photosArray.push(newPhoto);
   }
   return photosArray;
